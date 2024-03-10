90b9ba83de7ba171bd1bd625b8c714536c66d7b3
Shalu sharma <149373622+Shalu6634@users.noreply.github.com>
2024-03-10 19:32:20 +0530

    Add files via upload

---
+++ logic program/transpose_matrix.dart
@@ -0,0 +1,16 @@
+//24.Wap matrix convert into transpose matrix.
+
+import 'dart:io';
+
+void main() {
+  List l1 = [
+    [1, 2],
+    [3, 4]
+  ];
+
+  for (int i = 0; i < l1.length; i++) {
+    for (int j = 0; j < l1.length; j++) {
+      print("${l1[j][i]}");
+    }
+  }
+}
