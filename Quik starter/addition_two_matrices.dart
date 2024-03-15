9675efc91fec8c3a01098c41ec4c32abc325772b
Shalu sharma <149373622+Shalu6634@users.noreply.github.com>
2024-03-15 12:01:59 +0530

    addition_two_matrices.dart

--- Quik starter/addition_two_matrices.dart
+++ Quik starter/addition_two_matrices.dart
@@ -1,6 +1,37 @@
+//23.Wap to addition of two matrices di 3x3.
+
 import 'dart:io';
 
-void main()
-{
-
+void main() {
+  stdout.write("Enter n:");
+  int n = int.parse(stdin.readLineSync()!);
+  List l1 = [];
+  List l2 = [];
+  List sum = [];
+  for (int i = 0; i < n; i++) {
+    l1.add([]);
+    for (int j = 0; j < n; j++) {
+      stdout.write("Enter ${[i]}");
+      int a = int.parse(stdin.readLineSync()!);
+      l1[i].add(a);
+    }
+  }
+  print("....second array....");
+  for (int i = 0; i < n; i++) {
+    l2.add([]);
+    for (int j = 0; j < n; j++) {
+      stdout.write("Enter ${[i]}");
+      int a = int.parse(stdin.readLineSync()!);
+      l2[i].add(a);
+    }
+  }
+  sum.add([]);
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      sum[i].add(l1[i][j] + l2[i][j]);
+    }
+  }
+  print(l1);
+  print(l2);
+  print(sum);
 }
