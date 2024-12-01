83ed1736c28ab6e1049a9090f4e12c69be89e540
Shalu sharma <149373622+Shalu6634@users.noreply.github.com>
2024-12-01 20:00:00 +0530

    Update 1.dart

--- pattern/oop_question/1.dart
+++ pattern/oop_question/1.dart
@@ -1,37 +1,18 @@
-
-// Define a class named Rectangle with private attributes length and width.
-//Implement public member functions to set and get the values of these attributes.
-//Include a method to calculate the area of the rectangle.
-//Create an object of the Rectangle class and demonstrate the functionality of the implemented methods.
-
-import 'dart:io';
-
 class Rectangle {
   int? _length;
   int? _width;
 
-  void Setvalues() {
-    int length;
-    int width;
-
-    stdout.write("Enter the value of length : ");
-    length = int.parse(stdin.readLineSync()!);
-
+  void setvalues(int length, int width) {
     this._length = length;
-
-    stdout.write("Enter the value of width : ");
-    width = int.parse(stdin.readLineSync()!);
     this._width = width;
   }
 
   void getvalue() {
-
     print("The value of lenght : $_length");
     print("The value of width : $_width");
   }
 
   void areaOfRectangle() {
-
     // int b = _width;
 
     int area = _length! * _width!;
@@ -42,8 +23,12 @@ class Rectangle {
 
 void main() {
   Rectangle rectangle = Rectangle();
-
-  rectangle.Setvalues();
+  int length, width;
+//     stdout.write("Enter the value of length : ");
+//     length = int.parse(stdin.readLineSync()!);
+//     stdout.write("Enter the value of width : ");
+//     width = int.parse(stdin.readLineSync()!);
+  rectangle.setvalues(2, 3);
   rectangle.getvalue();
   rectangle.areaOfRectangle();
 }
